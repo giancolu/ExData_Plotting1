3976bac35e74607a0e330bef90e942a518255b70
GianLuca Colussi <gianluca@mint>
2014-07-10 13:59:21 +0200

    insert new files

--- plot4.R
+++ plot4.R
@@ -12,5 +12,5 @@ plot(db1$DateTime, db1$Sub_metering_1, type = "l", main = "", xlab = "", ylab="E
 points(db1$DateTime, db1$Sub_metering_2, type = "l", col = "blue")
 points(db1$DateTime, db1$Sub_metering_3, type = "l", col = "red")
 legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty = c(1,1,1), col = c(1,"red","blue"), bty = "n")
-plot(db1$DateTime, db1$Global_reactive_power, type = "l", main = "", xlab = "", ylab="Global_reactive_power")
+plot(db1$DateTime, db1$Global_reactive_power, type = "l", main = "", xlab = "datetime", ylab="Global_reactive_power")
 dev.off()
